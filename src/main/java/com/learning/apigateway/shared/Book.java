bb6d75f34813fa12abc854d6078fe9bd9f204a74
Nikhilnegi85 <neginikhil85@gmail.com>
2022-10-31 00:30:38 +0530

    initial commit

---
+++ src/main/java/com/learning/apigateway/shared/Book.java
@@ -0,0 +1,29 @@
+package com.learning.apigateway.shared;
+
+public class Book {
+
+	private int id;
+	private String bookName;
+	private String author;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+}
